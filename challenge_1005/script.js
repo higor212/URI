8459d8e5028ee204de58818ff2b241c4b551d41d
higor212 <higor2123456@gmail.com>
2021-04-03 22:12:17 -0300

    Resolvendo o desafio 1005

---
+++ challenge_1005/script.js
@@ -0,0 +1,9 @@
+const input = require('fs').readFileSync('./stdin', 'utf8');
+const value = input.split('\n');
+
+const valorA = parseFloat(value.shift())
+const valorB = parseFloat(value.shift())
+
+const media = ((valorA * 3.5) + (valorB * 7.5)) / 11
+console.log(`MEDIA = ${media.toFixed(5)}`)
+
