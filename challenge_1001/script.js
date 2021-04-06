b766c730f5dbfdf81fff9f4b5494bd1772494df0
higor212 <higor2123456@gmail.com>
2021-04-06 17:00:37 -0300

    Excluindo arquivos

--- challenge_1001/script.js
+++
@@ -1,9 +0,0 @@
-var input = require('fs').readFileSync('./dev/stdin', 'utf8');
-var values = input.split('\n');
-
-const valorA = parseInt(values.shift());
-const valorB = parseInt(values.shift());
-
-const soma = valorA + valorB;
-
-console.log('X = ' + soma)
